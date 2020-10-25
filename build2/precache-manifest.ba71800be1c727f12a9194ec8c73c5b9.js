cbb12af60991260c06c7f26fd7cb28eb04104b04
林辰洋 <44463097+foreststarsea@users.noreply.github.com>
2020-10-25 09:59:31 +0800

    Add files via upload

---
+++ build2/precache-manifest.ba71800be1c727f12a9194ec8c73c5b9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b125bcf8ff17905781c36d9f73ad0d09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "545d5d2a190400ff2c0e",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "965a2d6cf165359add76",
+    "url": "/static/css/main.92a3ebff.chunk.css"
+  },
+  {
+    "revision": "545d5d2a190400ff2c0e",
+    "url": "/static/js/2.b6d0ef1c.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.b6d0ef1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "965a2d6cf165359add76",
+    "url": "/static/js/main.622753fe.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  },
+  {
+    "revision": "8703f2de59cf9d6fae5f71958e7bbe24",
+    "url": "/static/media/banner.8703f2de.jpg"
+  }
+]);
